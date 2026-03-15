3c0fe404699424021934d38967a866de4c52da97
Marwin Buena <marwinfb@gmail.com>
2026-03-15 20:57:49 +0800

    Add clamp() to headlines

--- src/components/base/Input.jsx
+++ src/components/base/Input.jsx
@@ -8,7 +8,7 @@ const Input = () => {
       type="email"
       autoComplete="email"
       placeholder="Email address"
-      className="md:grow text-preset-6 block rounded-full px-5 py-3 outline-1 outline-secondary-blue-800 hover:outline-1 hover:outline-secondary-cyan-400 text-primary-neutral-000 hover:placeholder:text-primary-neutral-000 focus:outline-2 focus:outline-offset-3 focus:outline-secondary-red-4000 bg-secondary-blue-800 transition-all ease-in"
+      className="md:grow text-preset-6 block rounded-full px-5 py-3 outline-1 outline-secondary-blue-800 hover:outline-1 hover:outline-secondary-cyan-400 text-primary-neutral-000 hover:placeholder:text-primary-neutral-000 focus:outline-2 focus:outline-offset-3 focus:outline-secondary-red-400 bg-secondary-blue-800 transition-all ease-in"
     />
   );
 };
